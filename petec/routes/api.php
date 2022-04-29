1b8f1e3886ff67cd794b9706332c5d216e214b46
unknown <lucasantonio77ferreira@gmail.com>
2022-04-29 08:51:50 -0300

    Alterações no petec/routes/api.php

--- petec/routes/api.php
+++ petec/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// get - consulta
+// post - > cadastro
+// put - atualização (alteração)
+// delete - exclusão de dados
+
+Route::get('/pedidos','');
